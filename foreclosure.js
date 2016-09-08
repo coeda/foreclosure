f6dc9f0b43243202842ff4bf32a402177567df19
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-07 19:43:18 -1000

    added returnedObject that has getbalance, receivePayment, getMonthlyPayment, isForeClosed

--- foreclosure.js
+++ foreclosure.js
@@ -5,12 +5,44 @@ var month = 0;
 var monthsUntilEvicted;
 
 function loan(){
-  var account = new Object();
-    account.borrowed = 550000;
-    account.balance = 286000;
-    account.monthlyPayment = 1700;
-    account.defaulted = 0;
-    account.defaultsToForeclose = 5;
-    account.foreclosed = false;
+  var account = {
+    borrowed: 550000,
+    balance: 286000,
+    monthlyPayment: 1700,
+    defaulted: 0,
+    defaultsToForeclose: 5,
+    foreclosed: false,
+  };
 
+    function missPayment(){
+      account.defaulted += 1;
+      if(account.defaulted >= account.defaultsToForeclose){
+        account.foreclosed = true;
+      }
+    }
+
+    var returnedObject = {};
+
+      returnedObject.getBalance = function(){
+          return account.balance;
+      };
+
+      returnedObject.receivePayment = function(amount){
+        if(amount < account.monthlyPayment){
+          missPayment();
+          account.balance -= account.amount;
+        }
+      };
+
+      returnedObject.getMonthlyPayment = function(){
+        return account.monthlyPayment;
+      };
+
+      returnedObject.isForeClosed = function(){
+        return account.foreclosed;
+      };
+
+    return returnedObject;
 }
+
+stevesLoan = loan();
